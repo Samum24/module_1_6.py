dce3e1d93ecbf653a1b76bf2800790318f080cbe
Samum24 <miwikar@yandex.ru>
2024-10-28 23:12:42 +0200

    Add files via upload

--- module_1_6.py
+++ module_1_6.py
@@ -1,4 +1,4 @@
-
+from ftplib import print_line
 
 my_dict={'Michael':1974,'Alina':1997,'Viktor':1949}
 print(my_dict)
@@ -6,13 +6,18 @@ print(my_dict['Viktor'])
 print(my_dict.get('Ira'))
 my_dict.update(({'Ira':1976,'Katy':1954}))
 print(my_dict)
-del my_dict['Katy']
+print(my_dict.get('Kiril'))
+a = my_dict.pop('Katy')
+print(a)
 print(my_dict)
-my_set={5,8,3,8,2,'Kiwi',7,9,(3,1,2)}
+
+my_set={5,8,3,8,2,7,9,3,1,2}
 print(my_set)
-print(my_set.add(22))
+my_set.add('Kiwi')
+my_set.add('Cery')
 print(my_set)
-print(my_set.remove(8))
+my_set.discard('Cery')
+
 print(my_set)
 
 
